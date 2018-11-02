ef45f0bc9de0b03d72e7291ba1d956f2a2dc8939
Premprakash Yadav <premprakash.yadav@finouxad.com>
2018-11-02 19:45:37 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,32 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-// import firebase from 'firebase';
-// import { AngularFireModule } from 'angularfire2';
+import firebase from 'firebase';
+import { AngularFireModule } from 'angularfire2';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { VerificationPage } from '../pages/verification/verification';
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCKm86zTFd0KOkpcYYyJtG7jTeyzctSlHU",
+  authDomain: "com.surgician.surgician",
+  databaseURL: "https://surgician-73df1.firebaseio.com",
+  projectId: "surgician-73df1",
+  storageBucket: "surgician-73df1.appspot.com",
+  messagingSenderId: "708292051091"
+};
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    VerificationPage
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,4 +11,18 @@ export class HomePage {
 
   }
 
+  // signIn() {
+  //   // add a local variable to store navCtrl object
+  //   let thatNavCtrl = this.navCtrl;
+  //   //Step 1 — Pass the mobile number for verification
+  //   let tell = "+" + this.phone;
+  //   window.FirebasePlugin.verifyPhoneNumber(tell, 60, function (credential) {
+  //     let verificationId = credential.verificationId;
+  //     //This is STEP 2 — passing verification ID to verify Page
+  //     thatNavCtrl.push(VerificationPage, { verificationid: verificationId, phone: phoneNumber });
+  //   }, (error) => {
+  //     console.error(error);
+  //   });
+  // }
+
 }
